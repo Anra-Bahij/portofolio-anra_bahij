d3f5b9a0895c711856d4a1c0cc4e4bd8f2c97a78
Anra-Bahij <anrasyauqiybahij@gmail.com>
2025-05-03 23:06:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Inisialisasi animasi scroll
+AOS.init({
+  duration: 2500,
+  once: true
+});
+
+// Inisialisasi efek mengetik
+var typed = new Typed('#typed-text', {
+  strings: ["Hi, Saya Anra Syauqiy Bahij!", "Seorang Anak SMP.", "MARI BERKENALAN!"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 2500,
+  loop: true
+});
